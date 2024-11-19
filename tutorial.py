8c2536da4001d44835296c56909ab764379cb8ba
Anish Gudle <gudleanish@gmail.com>
2024-11-19 15:20:05 +0530

    3 commit

--- tutorial.py
+++ tutorial.py
@@ -130,8 +130,6 @@ def test_ai(config):
     game = PongGame(window, width, height)
     game.test_ai(winner, config)
 
-
-
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config.txt")
